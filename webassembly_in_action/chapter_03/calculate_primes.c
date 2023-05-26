0530127d78edd0d8541ea4e2f8bdf1cb01dd36d8
David Valente Santiago <david.valente.santiago@gmail.com>
2023-05-26 12:07:02 -0300

    update

---
+++ webassembly_in_action/chapter_03/calculate_primes.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <emscripten.h>
+
+int isPrime(int valor){
+    if(valor==2) return 1; // exceção
+    if(valor<=1 || valor%2==0) return 0; // menor que 1 ou valor par
+    for(int i=3;(i*i)<=valor;i+=2){ // percorre todos os valores intermediários
+        if(valor%i==0) return 0; // encontrou um valor divisível
+    }
+    return 1; // o valor e realmente primo
+}
+
+// função inicial do módulo webassembly
+int main(int argc, char const *argv[]){
+    int start=3;
+    int end=100000;
+    printf("Primos entre os valores %d e %d:\n",start,end);
+    for(int i=start;i <=end;i+=2){
+        if(isPrime(i)){
+            printf("%d",i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
